142edfcab3cea521955605317b6420354f29859e
unknown <carterpaccione@gmail.com>
2024-09-19 14:52:15 -0400

    edited portfolio page cards for linked hover effects

--- Main/src/components/pages/Portfolio.jsx
+++ Main/src/components/pages/Portfolio.jsx
@@ -7,18 +7,55 @@ export default function Portfolio () {
 
             <div className="container text-center">
                 <div className="row row-cols-2">
+                    
+                    <div className="col ">
+                        <img src='../src/images/README-thumbnail.png' alt='placeholderimage' className='thumbnail' id='proj1'></img>
+                            <div className='projectInfo'>
+                                <h3><a>README Generator</a></h3>
+                                <h3><a href='https://github.com/carterpaccione/README-Generator' target='_blank'>GitHub Repo</a></h3>
+                            </div>
+                    </div>
+                   
+                    <div className="col ">
+                        <img src='../src/images/EmployeeManager-thumbnail.png' alt='placeholderimage' className='thumbnail' id='proj1'></img>
+                            <div className='projectInfo'>
+                                <h3><a>Employee Manager</a></h3>
+                                <h3><a href='https://github.com/carterpaccione/Employee-Manager' target='_blank'>GitHub Repo</a></h3>
+                            </div>
+                    </div>
+                    
+                    <div className="col ">
+                        <img src='../src/images/WeatherDashboard-thumbnail.png' alt='placeholderimage' className='thumbnail' id='proj1'></img>
+                            <div className='projectInfo'>
+                                <h3><a href='https://weather-dashboard-81rv.onrender.com/' target='_blank'>Weather Dashboard</a></h3>
+                                <h3><a href='https://github.com/carterpaccione/Weather-Dashboard' target='_blank'>GitHub Repo</a></h3>
+                            </div>
+                    </div>
+
+                    <div className="col ">
+                        <img src='../src/images/VehicleBuilder-thumbnail.png' alt='placeholderimage' className='thumbnail' id='proj1'></img>
+                            <div className='projectInfo'>
+                                <h3><a>Vehicle Builder</a></h3>
+                                <h3><a href='https://github.com/carterpaccione/VehicleBuilder' target='_blank'>GitHub Repo</a></h3>
+                            </div>
+                    </div>
+
                     <div className="col ">
                         <img src='../src/images/placeholderimage.png' alt='placeholderimage' className='thumbnail' id='proj1'></img>
-                        </div>
-                    <div className="col">
-                        <img src='../src/images/placeholderimage.png' alt='placeholderimage' className='thumbnail'></img>
-                        </div>
-                    <div className="col">
-                        <img src='../src/images/placeholderimage.png' alt='placeholderimage' className='thumbnail'></img>
-                        </div>
-                    <div className="col">
-                        <img src='../src/images/placeholderimage.png' alt='placeholderimage' className='thumbnail'></img>
+                            <div className='projectInfo'>
+                                <h3><a>Inventory Manager</a></h3>
+                                <h3><a href='https://github.com/carterpaccione/Project-1' target='_blank'>GitHub Repo</a></h3>
+                            </div>
                     </div>
+
+                    <div className="col ">
+                        <img src='../src/images/ReactPortfolio-thumbnail.png' alt='placeholderimage' className='thumbnail' id='proj1'></img>
+                            <div className='projectInfo'>
+                                <h3><a>React Portfolio</a></h3>
+                                <h3><a href='https://github.com/carterpaccione/react-portfolio' target='_blank'>GitHub Repo</a></h3>
+                            </div>
+                    </div>
+                
                 </div>
             </div>
         </div>
